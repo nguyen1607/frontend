dcb539a9c896cdb142c603b22817426b994145c2
F2-TE-Mem-Nguyen.Tran <nguyen.tran@spclt.com.vn>
2021-02-25 11:26:26 +0700

    Update

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/node', function(req, res) {
+app.get('/', (req, res) => {
     res.send('Connected');
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
     console.log('Server listening on http://localhost:' + port);
 });
