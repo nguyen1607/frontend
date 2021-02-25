251c2c3c8762ce8666830c628ec186f6277a869b
F2-TE-Mem-Nguyen.Tran <nguyen.tran@spclt.com.vn>
2021-02-25 11:54:18 +0700

    Update

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,14 +1,21 @@
 const http = require('http');
 
+var routers = {
+    '/mode': (req, res) => {
+        res.writeHead(200);
+        res.end("<h1>Application</h1>");
+    }
+}
+
 function index (req, res) {
     res.writeHead(200);
     res.end('Connected!');
-}
+};
 
 http.createServer(function (req, res) {
     if (req.url === '/') {
         return index(req, res);
     }
     res.writeHead(404);
-    res.end(http.STATUS_CODES(404));
+    res.end(http.STATUS_CODES[404]);
 }).listen(3000);
